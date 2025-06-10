bdfd553ac5ef277248d73819cf14180df6d3d2b3
Abdullah Alnoime <abdullahalnoime@gmail.com>
2025-06-10 21:54:01 +0300

    update

--- src/components/quiz-components/option/Option.jsx
+++ src/components/quiz-components/option/Option.jsx
@@ -11,19 +11,21 @@ const Option = ({
     let baseClasses =
         "block w-full rounded border transition-all duration-150 my-2 px-4 text-center";
     let feedbackClass = "";
+
     if (showFeedback) {
         if (isCorrect) {
             feedbackClass =
-                "bg-green-100 border-green-600 text-green-800";
+                "bg-green-100 dark:bg-green-900 border-green-600 text-green-800 dark:text-green-200";
         } else if (index === selectedAnswer) {
             feedbackClass =
-                "bg-red-100 border-red-500 text-red-800";
+                "bg-red-100 dark:bg-red-900 border-red-500 text-red-800 dark:text-red-200";
         } else {
             feedbackClass =
                 "bg-gray-100 dark:bg-neutral-500 border-gray-300 text-gray-500 dark:text-neutral-100";
         }
     } else if (selectedAnswer === index) {
-        feedbackClass = "bg-blue-100 border-blue-500 text-blue-800";
+        feedbackClass =
+            "bg-blue-100 dark:bg-blue-900 border-blue-500 text-blue-800 dark:text-blue-200";
     } else {
         feedbackClass =
             "bg-white border-gray-300 dark:bg-neutral-700 hover:bg-blue-50 dark:hover:bg-neutral-600 hover:border-blue-300";
